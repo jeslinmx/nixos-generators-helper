acfdc94ed5f28ed7db06043fb250229d93e2d1ab
Jeshua Lin <jeslinmx@users.noreply.github.com>
2024-06-18 13:17:58 +0800

    added flake

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Utility function to define flake packages and nixosConfigurations for all nixos-generators formats";
+  inputs = {
+    nixos-generators.url = "github:nix-community/nixos-generators";
+    utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    nixos-generators,
+    utils,
+    ...
+  }:
+    {
+      lib.mkConfigWithImage = nixpkgs: modules:
+        (utils.lib.eachDefaultSystem (system: let
+          formatModules = nixpkgs.lib.filterAttrs (name: _: name != "all-formats") nixos-generators.nixosModules;
+        in {
+          packages =
+            builtins.mapAttrs (format: formatModule:
+              nixos-generators.nixosGenerate {
+                inherit system format;
+                modules = [formatModule] ++ modules;
+              })
+            formatModules
+            // {
+              nixosConfigurations = builtins.mapAttrs (format: formatModule:
+                nixpkgs.lib.nixosSystem {
+                  inherit system;
+                  modules = [formatModule] ++ modules;
+                })
+              formatModules;
+            };
+        }))
+        .packages;
+    }
+    // utils.lib.eachDefaultSystem (system: {formatter = nixpkgs.legacyPackages.${system}.alejandra;});
+}
