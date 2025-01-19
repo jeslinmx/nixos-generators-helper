1c4463e61da88f216ee41034a4b671ad66e3f097
Jeshua Lin <jeslinmx@users.noreply.github.com>
2025-01-20 02:43:23 +0800

    syntactic fix for build-image command

--- template/flake.nix
+++ template/flake.nix
@@ -13,49 +13,63 @@
     devshell.url = "github:numtide/devshell";
     devshell.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs: inputs.flake-parts.lib.mkFlake { inherit inputs; } ( { self, lib, ... }: {
-    imports = [ inputs.devshell.flakeModule ];
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake {inherit inputs;} ({
+      self,
+      lib,
+      ...
+    }: {
+      imports = [inputs.devshell.flakeModule];
 
-    flake = {
-      nixosModules = {
-        exampleModule = { system.stateVersion = "24.11"; };
+      flake = {
+        nixosModules = {
+          exampleModule = {system.stateVersion = "24.11";};
+        };
+        nixosConfigurations = {
+          example = inputs.nixpkgs.lib.nixosSystem {
+            system = "x86_64-linux";
+            modules = [
+              inputs.nixos-generators.nixosModules.proxmox-lxc
+              self.nixosModules.exampleModule
+            ];
+          };
+        };
       };
-      nixosConfigurations = {
-        example = inputs.nixpkgs.lib.nixosSystem {
-          system = "x86_64-linux";
-          modules = [
-            inputs.nixos-generators.nixosModules.proxmox-lxc
-            self.nixosModules.exampleModule
+
+      systems = ["x86_64-linux"];
+      perSystem = {
+        pkgs,
+        system,
+        ...
+      }: {
+        formatter = pkgs.alejandra;
+        devshells.default = {
+          commands = [
+            {
+              name = "build-image";
+              category = "build";
+              help = "Build <nixosConfiguration> [proxmox-lxc] image for [${system}]";
+              command = ''
+                ${inputs.nixos-generators.apps.${system}.nixos-generate.program} \
+                  --flake $PRJ_ROOT#''${1} \
+                  --format ''${2:-proxmox-lxc} \
+                  --system ''${3:-${system}} \
+                  --show-trace
+              '';
+            }
+            {
+              package = inputs.agenix.packages.${system}.default;
+              category = "dev";
+            }
+          ];
+          packages = [pkgs.nixd];
+          env = [
+            {
+              name = "RULES";
+              eval = "$PRJ_ROOT/secrets.nix";
+            }
           ];
         };
       };
-    };
-
-    systems = [ "x86_64-linux" ];
-    perSystem = { pkgs, system, ... }: {
-      formatter = pkgs.alejandra;
-      devshells.default = {
-        commands = [
-          {
-            name = "build-image";
-            category = "build";
-            help = "Build <nixosConfiguration> [proxmox-lxc] image for [${system}]";
-            command = ''
-              ${inputs.nixos-generators.apps.${system}.nixos-generate.program} \
-                --flake $PRJ_ROOT#''\${1} \
-                --format ''\${2:-proxmox-lxc} \
-                --system ''\${3:-${system}} \
-                --show-trace
-            '';
-          }
-          { package = inputs.agenix.packages.${system}.default; category = "dev"; }
-        ];
-        packages = [ pkgs.nixd ];
-        env = [
-          { name = "RULES"; eval = "$PRJ_ROOT/secrets.nix"; }
-        ];
-      };
-    };
-  });
+    });
 }
-
